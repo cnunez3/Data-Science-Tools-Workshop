e59547c810ab5c5f0ecd984d94f49e1779ef59e8
Fabien Forge <fabien.forge@gmail.com>
2021-10-07 14:02:04 -0400

    changed place

--- RStudio_Github_SetUp.R
+++
@@ -1,7 +0,0 @@
-library(usethis)
-library(gitcreds)
-create_github_token() # will open Github on your browser
-
-# In Github, click on generate token and copy paste the generated token
-
-gitcreds_set() # run this function and in the console below enter the token
